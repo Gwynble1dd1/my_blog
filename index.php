0e14f1d1d6d1630199ebb59bdff69f3d895143dc
Gwynble1dd <grivia456@gmail.com>
2024-07-15 11:37:02 +0300

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=my_site","root","");
+    echo "Connected Successfully <br>";
+    } catch (PDOException $e) {
+        echo "ERROR: -->" . $e->getMessage();
+        die();
+    }
+
+    $name = 'Hacker';
+
+    $update_query = "UPDATE users SET age = 99 where name = :username"; // просто создаем переменную с запросом
+    $prepared_update_query = $conn->prepare($update_query); // подготавливаем запрос
+
+    $prepared_update_query->execute(['username'=>$name]);
+
 
 ?>
